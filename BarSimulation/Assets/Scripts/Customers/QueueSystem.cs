fc7ad237e56a3734b97e652568e7d790176baca1
WillSucksAtCode <william.heath@ontariotechu.net>
2022-11-08 14:06:25 -0500

    Pathing and some of the final models
    bababooey

---
+++ BarSimulation/Assets/Scripts/Customers/QueueSystem.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class QueueSystem : MonoBehaviour
+{
+    public List<GameObject> barQ = new List<GameObject>();
+    public List<GameObject> waitQ = new List<GameObject>();
+    public List<GameObject> chairQ = new List<GameObject>();
+    public List<Transform> barQueuePos = new List<Transform>();
+    public List<Transform> waitQueuePos = new List<Transform>();
+    public List<Transform> chairQueuePos = new List<Transform>();
+
+    public GameObject customer;
+    public Transform customerStart;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            AddToQueue(barQueuePos, barQ, Instantiate(customer, customerStart.position, customerStart.rotation));
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            AddToQueue(waitQueuePos, waitQ, RemoveFront(barQueuePos, barQ));
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AddToQueue(chairQueuePos, chairQ, RemoveFront(waitQueuePos, waitQ));
+        }
+    }
+
+    public bool AddToQueue(List<Transform> list, List<GameObject> objs, GameObject newobj)
+    {
+        if (objs.Count >= list.Count || newobj == null)    return false;
+
+        newobj.GetComponent<NavMeshAgent>().destination = list[objs.Count].position;
+        objs.Add(newobj);
+
+        return true;
+    }
+
+    public GameObject RemoveFront(List<Transform> list, List<GameObject> objs)
+    {
+        if (objs.Count == 0) return null;
+
+        GameObject obj = objs[0];
+        objs.RemoveAt(0);
+        CalcPos(list, objs);
+        return obj;
+    }
+
+    public void CalcPos(List<Transform> list, List<GameObject> objs)
+    {
+        int i = 0;
+        foreach (GameObject obj in objs)
+        {
+            NavMeshAgent agent = obj.GetComponent<NavMeshAgent>();
+
+            //set targets here
+            agent.destination = list[i++].position;
+        }
+    }
+}
---
+++ BarSimulation/Assets/Scripts/Customers/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Timer : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private float timePassed = 0;
+    public float angerMeter;
+    public Slider slider;
+    public GameObject angerUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        angerUI.SetActive(false);
+
+        slider.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            timePassed += Time.deltaTime;
+            slider.value = CalculateTime();
+            angerUI.SetActive(true);
+        }
+
+        if (angerMeter <= timePassed)
+        {
+            Debug.Log("Im pissed!");
+        }
+
+        if (agent.pathPending)
+        {
+            timePassed = 0;
+            angerUI.SetActive(false);
+        }
+    }
+
+    float CalculateTime()
+    {
+        return timePassed / angerMeter;
+    }
+
+}
