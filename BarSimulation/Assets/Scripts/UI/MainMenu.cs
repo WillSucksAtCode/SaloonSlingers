d95741213ddcc6265c2c92e482c11b525a862963
James <james.accinelli@ontariotechu.net>
2022-11-09 00:54:38 -0500

    pause, win/loss scenes added will polish tmrw

--- BarSimulation/Assets/Scripts/UI/MainMenu.cs
+++ BarSimulation/Assets/Scripts/UI/MainMenu.cs
@@ -5,26 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public bool newGame;
-
     public void PlayGame()
     {
-        SceneManager.LoadScene(""); // will change this to whatever our scene is called later
-        newGame = true;
+        SceneManager.LoadScene("FullScene");
     }
-
-    /*
-    public void Tutorial()
-    {
-        SceneManager.LoadScene("Tutorial");
-    }
-    */
 
     // function to close the game
     public void QuitGame()
     {
         Debug.Log("Exiting Game..."); // to show it works without needing to build
         Application.Quit(); // this only works once the game is built
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
 
